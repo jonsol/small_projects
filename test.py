d40f39c8f9994e123a5b0bbf8b7980a149c4a188
jonsol <django.palmer@gmail.com>
2025-09-30 20:20:34 +0200

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+
+import tensorflow as tf
+
+
